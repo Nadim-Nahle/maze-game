eca09c6f75672d8b172ee021eac1ee353ba983a8
Nadim <nahlenadim@gmail.com>
2022-05-22 16:24:29 +0300

    creating function to return the boundaries to their initial color

--- game.js
+++ game.js
@@ -1,36 +1,43 @@
-var boundary = document.getElementsByClassName("boundary");
-var start = document.getElementById("start");
-var end = document.getElementById("end");
-var qiqi = document.getElementById("status");
+window.addEventListener("load", function(){
+
+    var boundary = document.getElementsByClassName("boundary");
+    var start = document.getElementById("start");
+    var end = document.getElementById("end");
+    var WinOrLoss = document.getElementById("status");
 
 
 //function to change the background color to red
-function changeColor(){
-    for (var i=0 ; i<5; i++){
-        boundary[i].style.backgroundColor = "red";
+    function changeColor(){
+        for (var i=0 ; i<5; i++){
+         boundary[i].style.backgroundColor = "red";
+        }
     }
-}
 
-function startGame(){
-    start.addEventListener("click", Launch);
-    winGame();  
+    function ReturnColor(){
+        for (var i=0 ; i<5; i++){
+         boundary[i].style.backgroundColor = "#eeeeee";
+        }
+    }
+
+    function startGame(){
+        start.addEventListener("click", Launch);
+        winGame();  
      
-}
-
-function Launch(){
-    for (var i = 0; i<5; i++){
-        boundary[i].addEventListener("mouseover", changeColor);
     }
-}
 
-function winGame(){
-    end.addEventListener("mouseover", (event) =>{
-       qiqi.innerHTML = "You Win !!"
-    })
- }
+    function Launch(){
+    for (var i = 0; i<5; i++){
+            boundary[i].addEventListener("mouseover", changeColor);
+        }
+    }
 
- startGame();
- 
+    function winGame(){
+        end.addEventListener("mouseover", (event) =>{
+        WinOrLoss.innerHTML = "You Win !!"
+        })
+    };
 
+    startGame();
 
+})
 
